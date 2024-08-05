c3e8aadc4fd0b09399270cec2b3dba5275bfffb6
MisbahProgrammer <misbahrehman891@gmail.com>
2024-08-05 08:09:40 +0500

    Bg-changer

---
+++ script.js
@@ -0,0 +1,17 @@
+let container = document.getElementById('screen');
+let red = document.getElementById('red');
+let blue = document.getElementById('blue');
+let green = document.getElementById('green');
+let yellow = document.getElementById('yellow');
+function redColor (){
+    container.style.backgroundColor = 'red';
+}
+function blueColor (){
+    container.style.backgroundColor = 'blue';
+}
+function greenColor (){
+    container.style.backgroundColor = 'green';
+}
+function yellowColor (){
+    container.style.backgroundColor = 'yellow';
+}
